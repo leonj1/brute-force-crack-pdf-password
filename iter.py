726e8cdceca040db68de584da5a50c6c5344dd78
Jose Leon <leonj1@gmail.com>
2017-11-05 09:00:42 -0500

    updates

--- iter.py
+++ iter.py
@@ -4,7 +4,7 @@ def perm(n, seq):
     for p in itertools.product(seq, repeat=n):
         print("".join(p))
 
-perm(4, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 #perm(4, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-#perm(4, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+perm(5, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+perm(6, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
